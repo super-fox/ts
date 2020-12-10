d4f1b98b88b065378e06c8b51466fefd12d26ead
songyiLing_1 <super1031syl@163.com>
2020-12-10 15:17:28 +0800

    g

---
+++ 02-变量.js
@@ -0,0 +1,6 @@
+var names = ['古力娜扎', '迪丽热巴', '巴尔扎哈'];
+console.log(names);
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+var grounds = new Array();
---
+++ 02-变量.ts
@@ -0,0 +1,16 @@
+// let names: string[] = ['古力娜扎', '迪丽热巴', '巴尔扎哈']
+// console.log(names)
+// for (let i: number = 0; i < names.length; i++) {
+//     console.log(names[i])
+// }
+
+// let grounds: string[] = new Array()
+
+let sum: number = 0
+let nums: number[] = [1, 2, 3]
+for (let i: number = 0; i < nums.length; i++) {
+    for (let j: number = 0; j < i; j++) {
+
+    }
+}
+console.log(sum)
